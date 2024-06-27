f2af003bdccfd260b89dcced37e0750ffab1499d
Erenikus Git <54367040+Erenikus@users.noreply.github.com>
2024-06-27 23:26:49 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+total_lesson=12
+total_time=1.5
+name_course='Python'
+print('Курс: ',name_course,'всего задач:',total_lesson,', затрачено часов: ',total_time,'среднее время выполнения ',total_time / total_lesson,' часа.')
